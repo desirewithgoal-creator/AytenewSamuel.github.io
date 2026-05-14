5c0aa932181c7dd5b5fae99672789fec8616f767
desirewithgoal-creator <desirewithgoal@gmail.com>
2026-05-14 13:38:54 +0300

    Create tailwind.config.ts

---
+++ lib/tailwind.config.ts
@@ -0,0 +1,16 @@
+// tailwind.config.ts
+export default {
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0B2B44',
+        gold: '#E7A800',
+        'off-white': '#F5F7FA',
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
+        inter: ['var(--font-inter)'],
+      },
+    },
+  },
+};
